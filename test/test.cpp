17ec9bbb48f21c95458894027be5a4c8382bc6f1
Abhimanyu Saxena <abhi.manyu1225@gmail.com>
2023-10-05 18:03:22 -0400

    Added additional test case

--- test/test.cpp
+++ test/test.cpp
@@ -6,3 +6,9 @@ TEST(pidcontroller, Test_Case_1) {
   double error = 5.0;
   EXPECT_DOUBLE_EQ(controller.calculate(error), 5.0);
 }
+
+TEST(pidcontroller, Test_Case_2) {
+  PIDController controller(-0.2, 5.2, 0.0);
+  double error = 2.0;
+  EXPECT_DOUBLE_EQ(controller.calculate(error), 2.0);
+}
