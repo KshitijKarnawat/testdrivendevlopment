70a9493cf17b0c68f7fb5f5a4612daaf47317a83
lorocks <lowell.lobo@hotmail.com>
2023-10-05 20:58:52 -0400

    Add PID implementation

--- libs/pidcontroller/pidcontroller.cpp
+++ libs/pidcontroller/pidcontroller.cpp
@@ -3,15 +3,22 @@
 
 double PIDController::calculate(double error) {
   double output = 0.0;
+  std::cout << kp*error << std::endl;
+  std::cout << ki*integral(error) << std::endl;
+  std::cout << kd*derivative(error) << std::endl;
+  output = (kp*error) + (ki*integral(error)) + (kd*derivative(error));
+  previous_error = error;
   return output;
 }
 
 double PIDController::integral(double error) {
-  double integral = 0.0;
+  double integral;
+  integral = error * dt;
   return integral;
 }
 
 double PIDController::derivative(double error) {
-  double derivative = 0.0;
+  double derivative;
+  derivative = (error - previous_error)/ dt;
   return derivative;
 }
