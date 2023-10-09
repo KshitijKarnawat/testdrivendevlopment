08fa33ffaa04085dd1ab8a184a7b8bb994599c50
Lowell Lobo <63993526+lorocks@users.noreply.github.com>
2023-10-09 15:42:54 -0400

    Delete build directory
    Not necessary directory build gets deleted

--- build/test/cpp-test[1]_tests.cmake
+++
@@ -1,5 +0,0 @@
-add_test( pidcontroller.Test_Case_1 /home/lorocks/hw_folder/testdrivendevlopment/build/test/cpp-test [==[--gtest_filter=pidcontroller.Test_Case_1]==] --gtest_also_run_disabled_tests)
-set_tests_properties( pidcontroller.Test_Case_1 PROPERTIES WORKING_DIRECTORY /home/lorocks/hw_folder/testdrivendevlopment/build/test SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-add_test( pidcontroller.Test_Case_2 /home/lorocks/hw_folder/testdrivendevlopment/build/test/cpp-test [==[--gtest_filter=pidcontroller.Test_Case_2]==] --gtest_also_run_disabled_tests)
-set_tests_properties( pidcontroller.Test_Case_2 PROPERTIES WORKING_DIRECTORY /home/lorocks/hw_folder/testdrivendevlopment/build/test SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set( cpp-test_TESTS pidcontroller.Test_Case_1 pidcontroller.Test_Case_2)
